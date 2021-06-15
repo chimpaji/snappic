82c461e22b5bdf83c534a79c3d8e0f5b272681e3
chimpaji <49395962+chimpaji@users.noreply.github.com>
2021-06-15 19:26:42 +0700

    CSS fixed (white border and no white border)

--- src/Body.js
+++ src/Body.js
@@ -8,10 +8,51 @@ export default function Body() {
   return (
     <div className="flex-grow  flex justify-center items-center">
       <div className="inline-flex w-full space-x-5  overflow-x-scroll h-full items-center xs:no-scrollbar">
-        {imageSrc.length > 0 ? (
+        <>
+          <UploadMoreButton />
+
+          <div className="flex flex-shrink-0 space-x-5 items-center ">
+            <div className="relative z-10 w-48 h-48 flex justify-center items-center">
+              <img
+                id="nowhiteimg"
+                className="absolute w-48 h-48"
+                src="/img/exampleImage/dog2.jpg"
+                alt="crop"
+              />
+
+              <img
+                id="border"
+                className="absolute"
+                src={`/img/borderSingleImage/black.svg`}
+                alt="1"
+              />
+            </div>
+
+            <div className="relative z-10 w-48 h-48 flex justify-center items-center">
+              <div className="relative w-48 h-48 ">
+                <img
+                  id="whiteimg"
+                  className="absolute w-32 h-32"
+                  src="/img/exampleImage/dog1.jpg"
+                  alt="crop"
+                />
+
+                <img
+                  className="absolute"
+                  src={`/img/borderSingleImage/black.svg`}
+                  alt="1"
+                />
+              </div>
+            </div>
+          </div>
+          <UploadMoreButton />
+        </>
+
+        {/* Comment out the funcitonal */}
+        {/* {imageSrc.length > 0 ? (
           <>
             <UploadMoreButton />
-            {/* map over the imageSrcCropped */}
+            
             <div className="flex flex-shrink-0 space-x-5 items-center">
               {imageSrcCropped.map((element) => (
                 <SingleImage id={element.id} key={element.id} />
@@ -21,7 +62,7 @@ export default function Body() {
           </>
         ) : (
           <UploadMoreButtonAnimation />
-        )}
+        )} */}
       </div>
     </div>
   );
